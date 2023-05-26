700d1e932c6dfe61f038757b8938c5a458054c2c
doodoji <doodozik@gmail.com>
2023-05-26 21:44:51 +0900

    add search highlight

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   var options = {
-    valueNames: ["title", "rating"],
+    valueNames: ["title", "author", "rating"],
   };
 
   var list = new List("webtoonList", options);
@@ -31,9 +31,56 @@ window.onload = function () {
   }
 
   webtoonList.forEach((li) => li.addEventListener("click", selectWebtoon));
-  console.log(webtoonList);
 
   // 첫 번째 웹툰 선택
   const firstWebtoon = webtoonList[0];
   firstWebtoon.click();
+
+  const query = document.querySelector("input.search");
+  const main = document.querySelector("ul.list");
+
+  // Find all text nodes in the main element.
+  const treeWalker = document.createTreeWalker(main, NodeFilter.SHOW_TEXT);
+  const allTextNodes = [];
+  let currentNode = treeWalker.nextNode();
+  while (currentNode) {
+    allTextNodes.push(currentNode);
+    currentNode = treeWalker.nextNode();
+  }
+
+  query.addEventListener("input", () => {
+    CSS.highlights.clear();
+
+    const str = query.value.trim().toLowerCase();
+    if (!str) {
+      return;
+    }
+
+    const ranges = allTextNodes
+      .map((el) => {
+        return { el, text: el.textContent.toLowerCase() };
+      })
+      .filter(({ text }) => text.includes(str))
+      .map(({ text, el }) => {
+        // Find all instances of str in el.textContent
+        const indices = [];
+        let startPos = 0;
+        while (startPos < text.length) {
+          const index = text.indexOf(str, startPos);
+          if (index === -1) break;
+          indices.push(index);
+          startPos = index + str.length;
+        }
+
+        return indices.map((index) => {
+          const range = new Range();
+          range.setStart(el, index);
+          range.setEnd(el, index + str.length);
+          return range;
+        });
+      });
+
+    const highlight = new Highlight(...ranges.flat());
+    CSS.highlights.set("search-result-highlight", highlight);
+  });
 };
